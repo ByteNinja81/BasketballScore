5a240d2462aca147ad51e5e1731563fb306ff486
ByteNinja81 <141329716+ByteNinja81@users.noreply.github.com>
2025-08-30 14:31:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+let home = document.getElementById("home-score")
+let guest = document.getElementById("guest-score")
+
+let homeScore = home.textContent
+let guestScore = guest.textContent
+
+function addOneHome() {
+  homeScore = +homeScore +1
+  home.textContent = homeScore
+}
+
+function addTwoHome() {
+  homeScore = +homeScore +2
+  home.textContent = homeScore
+}
+
+function addThreeHome() {
+  homeScore = +homeScore +3
+  home.textContent = homeScore
+}
+
+function addOneGuest() {
+  guestScore = +guestScore +1
+  guest.textContent = guestScore
+}
+
+function addTwoGuest()  {
+  guestScore = +guestScore +2
+  guest.textContent = guestScore
+}
+
+function addThreeGuest()  {
+  guestScore = +guestScore +3
+  guest.textContent = guestScore;
+}
+
+function newGame()  {
+  homeScore = 0;
+  guestScore = 0;
+  home.textContent = homeScore;
+  guest.textContent = guestScore;
+}
+
